c1d93a5fb2cbdd651297af7425e557410f27e0ed
jgongiwala <117845441+jgongiwala@users.noreply.github.com>
2023-06-15 16:09:03 +0000

    add html file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['search_query']
+    # Perform search logic here
+    return f'Searching for: {query}'
 
 if __name__ == '__main__':
     app.run()
-    
