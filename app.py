0ed98a79fd1cce0139496aa047543b7901312e5d
jgongiwala <117845441+jgongiwala@users.noreply.github.com>
2023-06-15 16:17:17 +0000

    chat gpt integration

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import openai
 from flask import Flask, render_template, request
+
+# Set up your OpenAI API key
+openai.api_key = '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'
 
 app = Flask(__name__)
 
@@ -9,8 +13,23 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['search_query']
-    # Perform search logic here
-    return f'Searching for: {query}'
+
+    # Use ChatGPT for search response
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=f"Search: {query}\n",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    search_result = response.choices[0].text.strip()
+
+    return render_template('index.html', search_result=search_result)
 
 if __name__ == '__main__':
     app.run()
